7c62505a53031bc5f3b318687d4d7dc3c33dc019
Sn0flake <215378748+Sn0flake@users.noreply.github.com>
2026-04-26 13:20:08 +0800

    Site updated: 2026-04-26 13:19:49

---
+++ comments/giscus.js
@@ -0,0 +1,36 @@
+(() => {
+    const loadComments = async () => {
+        const giscusContainer = document.getElementById('giscus_container');
+        if (!giscusContainer) return;
+
+        // 清空 iframe
+        while (giscusContainer.firstChild) {
+            giscusContainer.removeChild(giscusContainer.firstChild);
+        }
+
+        // 重建 script
+        const script = document.createElement('script');
+        script.src = 'https://giscus.app/client.js';
+        script.setAttribute('data-repo', 'Sn0wflakeQWQ/Pages');
+        script.setAttribute('data-repo-id', 'R_kgDOSMLkPQ');
+        script.setAttribute('data-category', 'Announcements');
+        script.setAttribute('data-category-id', 'DIC_kwDOSMLkPc4C7qJO');
+        script.setAttribute('data-mapping', 'pathname');
+        script.setAttribute('data-strict', '0');
+        script.setAttribute('data-reactions-enabled', '1');
+        script.setAttribute('data-emit-metadata', '0');
+        script.setAttribute('data-input-position', 'bottom');
+        script.setAttribute('data-theme', 'preferred_color_scheme');
+        script.setAttribute('data-lang', 'zh-CN');
+        script.setAttribute('crossorigin', 'anonymous');
+        script.setAttribute('async', '');
+
+        giscusContainer.appendChild(script);
+    };
+
+    // 載入＆pjax 重新掛載
+    window.loadComments = loadComments;
+    window.addEventListener('pjax:success', () => {
+        window.loadComments = loadComments;
+    });
+})();
